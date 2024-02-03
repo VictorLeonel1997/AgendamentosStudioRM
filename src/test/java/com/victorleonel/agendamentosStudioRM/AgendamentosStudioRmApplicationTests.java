0899ee7bac1aac4feee35a57d3b285f99da93992
Victor Leonel <victorps2.hotmail.com@gmail.com>
2024-02-02 23:01:20 -0300

    First Commit

---
+++ src/test/java/com/victorleonel/agendamentosStudioRM/AgendamentosStudioRmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorleonel.agendamentosStudioRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosStudioRmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
