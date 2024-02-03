78495295632caf55ec790b700882e889a11f5e5e
Victor Leonel <victorps2.hotmail.com@gmail.com>
2024-02-02 23:04:36 -0300

    First Commit

--- src/main/java/com/victorleonel/agendamentosStudioRM/AgendamentosStudioRmApplication.java
+++ src/main/java/com/victorleonel/agendamentosStudioRM/AgendamentosStudioRmApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AgendamentosStudioRmApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AgendamentosStudioRmApplication.class, args);
 	}
 
